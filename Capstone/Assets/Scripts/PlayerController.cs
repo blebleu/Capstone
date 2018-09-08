077ffba222e5b646d42e816e6d10bd26f858980a
Unknown <blebleu4@gmail.com>
2018-09-07 21:50:15 -0500

    FPS Controller Shoots
    FPS controller now shoots with raycasting and enemy health is changed

--- Capstone/Assets/Scripts/PlayerController.cs
+++ Capstone/Assets/Scripts/PlayerController.cs
@@ -2,15 +2,83 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public int gunDamage = 1;
+    public float fireRate = 0.25f;
+    public float weaponRange = 50f;
+    public float hitForce = 100f;
+    public Transform gunEnd;
+
+    private Camera fpsCam;
+    private WaitForSeconds shotDuration = new WaitForSeconds(0.07f);
+    private AudioSource gunAudio;
+    private LineRenderer laserLine;
+    private float nextFire;
+
+
+    void Start()
+    {
+        laserLine = GetComponent<LineRenderer>();
+
+        gunAudio = GetComponent<AudioSource>();
+
+        fpsCam = GetComponentInChildren<Camera>();
+
+    
+    }
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+
+            StartCoroutine(ShotEffect());
+
+            Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0.0f));
+
+            RaycastHit hit;
+
+            laserLine.SetPosition(0, gunEnd.position);
+
+            if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, weaponRange))
+            {
+                laserLine.SetPosition(1, hit.point);
+
+                if(hit.transform.tag == "Enemy")
+                {
+                    EnemyController theEnemy = hit.collider.GetComponent <EnemyController>();
+
+                    theEnemy.Damage(gunDamage);
+                    
+
+                }
+                
+
+                if (hit.rigidbody != null)
+                {
+                    hit.rigidbody.AddForce(-hit.normal * hitForce);
+                }
+            }
+            else
+            {
+                laserLine.SetPosition(1, rayOrigin + (fpsCam.transform.forward * weaponRange));
+            }
+        }
+    }
+
+
+    private IEnumerator ShotEffect()
+    {
+        gunAudio.Play();
+
+        laserLine.enabled = true;
+
+        yield return shotDuration;
+
+        laserLine.enabled = false;
+    }
+
 }
