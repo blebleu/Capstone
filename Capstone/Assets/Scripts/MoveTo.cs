fa7fa7d0423f4b867a403e5361c31710707ccf3a
Unknown <blebleu4@gmail.com>
2018-10-07 21:30:34 -0500

    Stashing Work

---
+++ Capstone/Assets/Scripts/MoveTo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTo : MonoBehaviour {
+
+
+    public Transform player;
+    public Transform camera;
+    enum EnemyStates { Patrol, Seeking, Attacking, Retreating };
+    EnemyStates enemyState;
+    EnemyController enemyScript;
+
+    private void Start()
+    {
+        EnemyController enemyScript = GetComponent<EnemyController>();
+    }
+    private void Update()
+    {
+        /***if (enemyScript.currentState = (int)enemyState.Patrol)
+        {
+
+        }
+        ***/
+
+        UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.destination = player.position;
+    }
+}
